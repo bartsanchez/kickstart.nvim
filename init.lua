5b7c6469a1ddaf78aa30a2c7e9299105e704a70f
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2024-12-03 22:14:29 +0100

    feat(bart): enable Ruff lsp

--- init.lua
+++ init.lua
@@ -674,6 +674,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
